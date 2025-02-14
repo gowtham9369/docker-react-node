f6c8c2eb0f4af8562940361e49c40ec2a7b16ae1
gowtham <gowtham9369@gmail.com>
2025-02-14 17:22:00 +0100

    update capitalize utils as helper function

--- FE/FleetView/src/components/VehicleStatus.tsx
+++ FE/FleetView/src/components/VehicleStatus.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Chip } from '@mui/material';
 import { VehicleStatusProps } from '../types/vehicle';
+import {capitalizeString} from '../utils/capitalizeString';
 
 const VehicleStatus: React.FC<VehicleStatusProps> = ({ state }) => {
   // Determine the chip color based on the vehicle state
-  const chipColor = state === 'ACTIVE' ? 'default' : 'warning';
+  const chipColor = state === 'ACTIVE' ? 'warning' : 'default';
 
   return (
     <Chip 
-      label={state} 
-      color={chipColor} 
-      sx ={{ color: 'black', borderRadius: 1, textTransform: 'capitalize' }}
+      label={capitalizeString(state)}  // Apply formatting
+      color={chipColor}
+      style={{backgroundColor: state === 'ACTIVE' ?'#FFFCF0' :"#F1F2F4"}} // Add custom background color
+      className="vehicle-status"
+      sx={{ color: 'black', borderRadius: 1 }}
       size="small"
       variant="outlined"
     />
